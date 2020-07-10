6ed1753de54928714f1951e79c707a82f222e2b7
dev09 <53975137+devdevdev09@users.noreply.github.com>
2020-07-11 02:33:31 +0900

    Update DemoApplication.java

--- src/main/java/com/docker/example/docker/DemoApplication.java
+++ src/main/java/com/docker/example/docker/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	public String dockerHello(){
-		return "indextest";
+		return "indextestscript";
 	}
 
 	public static void main(String[] args) {
