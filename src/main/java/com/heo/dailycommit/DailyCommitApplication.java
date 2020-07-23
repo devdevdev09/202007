cd0666224278720b4ee4859604d16004ea212acf
dev09 <devdevdevdevdev09@gmail.com>
2020-07-23 16:17:17 +0900

    remove unused code

--- src/main/java/com/heo/dailycommit/DailyCommitApplication.java
+++ src/main/java/com/heo/dailycommit/DailyCommitApplication.java
@@ -1,17 +1,10 @@
 package com.heo.dailycommit;
 
-import com.heo.dailycommit.main.DailyCommit;
-import com.heo.dailycommit.slack.Slack;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -20,9 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DailyCommitApplication {
 
 	Logger logger = LoggerFactory.getLogger(this.getClass());
-
-	@Autowired
-	private DailyCommit dailyCommit;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DailyCommitApplication.class, args);
