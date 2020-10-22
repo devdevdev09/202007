fec4df7cbdababcef0dd78b0c50441b14031f5cb
dev09 <devdevdevdevdev09@gmail.com>
2020-10-22 17:30:04 +0900

    restdoc 추가중 ....

--- src/main/java/com/heo/dailycommit/restcontroller/RestMainController.java
+++ src/main/java/com/heo/dailycommit/restcontroller/RestMainController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/test")
+@RequestMapping("/")
 @RestController
 public class RestMainController {
     Logger logger = LoggerFactory.getLogger(this.getClass());
--- src/test/java/com/heo/dailycommit/restController/RestMainControllerTest.java
+++ src/test/java/com/heo/dailycommit/restController/RestMainControllerTest.java
@@ -5,45 +5,84 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
-// @AutoConfigureWebClient
-public class RestMainControllerTest {
-    
-    @Autowired
-    MockMvc mvc;
+// import static
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-    @Test
-    public void test_stringTest() throws Exception{
-        mvc.perform(get("/test/string")).andExpect(status().isOk()).andExpect(content().string("test string"));
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.mockito.ArgumentMatchers.isNotNull;
+import static org.hamcrest.CoreMatchers.is;
+
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RestMainControllerTest {
+
+    @Rule
+    public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("custom");
+    
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Before
+    public void setUp(){
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+                    .apply(documentationConfiguration(this.restDocumentation))
+                    .build();
     }
 
-    // @Test
-    // public void test_stringTest(@Autowired WebTestClient webClient) {
-    //     webClient.get().uri("/test/string").exchange().expectStatus().isOk().expectBody(String.class).isEqualTo("test string");
-    // }
+    @Test
+    public void GET_DAILY_COMMIT() throws Exception {
+        mockMvc.perform(
+            get("/dailycommit/{id}", "devdevdev09")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("user", is(notNullValue())))
+                .andExpect(jsonPath("date", is(notNullValue())))
+                .andExpect(jsonPath("daily", is(notNullValue())))
+                .andExpect(jsonPath("continues", is(notNullValue())))
+                .andDo(document("index"));
+
+                // .andExpect(jsonPath("user", is(notNullValue())
+                // .andExpect(jsonPath("date", is(notNullValue())
+                // .andExpect(jsonPath("continue", is(notNullValue()));
+    }
 
 
 
-    // @Test
-    // public void test_stringTest(@Autowired WebTestClient webClient) {
-    //     webClient.get().uri("/test/string").exchange().expectStatus().isOk().expectBody(String.class).isEqualTo("test string");
-    // }
+
+
+
+
+    
 
 }
